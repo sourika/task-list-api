f71b090060d91cc44f1628ce2682ed3a24d60328
Nadzeya Nialiubina <nelyubina.nadia@gmail.com>
2025-10-31 17:45:13 -0700

    Implement task model and routes with CRUD operations

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,32 @@
+from datetime import datetime
+from sqlalchemy import String, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str | None] = mapped_column(String(200), nullable=True)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    completed_at: Mapped[datetime | None] = mapped_column(DateTime(), nullable=True, default=None)
+
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": False,
+        }
+
+    @staticmethod
+    def from_dict(data: dict):
+        if "title" not in data:
+            raise KeyError("title")
+        if "description" not in data:
+            raise KeyError("description")
+    
+        return Task(
+                title=data["title"],
+                description=data["description"],
+                completed_at=None,
+            )        
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,96 @@
-from flask import Blueprint
+from flask import Blueprint, Response, abort, make_response, request
+from app.models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# helpers
+def get_task_by_id(task_id):
+    try:
+        task_id = int(task_id)
+    except (TypeError, ValueError):
+        return abort(make_response({"message": f"Task id {task_id} invalid"}, 400))
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+    if not task:
+        return abort(make_response({"message": f"Task id {task_id} not found"}, 404))
+
+    return task
+
+
+def create_instance_from_dict(data: dict, factory):
+    try:
+        instance = factory(data)              
+    except (KeyError, ValueError):
+        return abort(make_response({"details": "Invalid data"}, 400))
+    db.session.add(instance)
+    db.session.commit()
+    return instance, 201    
+
+
+def parse_json_object():
+    """Return dict or 400 {'details': 'Invalid data'}."""
+    data = request.get_json(silent=True) # don’t raise BadRequest on invalid JSON — just return None.
+    if not isinstance(data, dict):
+        abort(make_response({"details": "Invalid data"}, 400))
+    return data
+
+
+# routes
+@tasks_bp.post("")
+def create_task():
+    data = parse_json_object() 
+    task, status = create_instance_from_dict(data, Task.from_dict)
+    return task.to_dict(), status
+
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    # title_param = request.args.get("title")
+    # if title_param:
+    #     query = query.where(Task.title.ilike(f"%{title_param}%"))
+    
+    # description_param = request.args.get("description")
+    # if description_param:
+    #     query = query.where(Task.description.ilike(f"%{description_param}%"))
+    
+    # query = query.order_by(Task.id)
+    # tasks = db.session.scalars(query)
+
+    # tasks_response = []
+    # for task in tasks:
+    #     tasks_response.append(task.to_dict())
+        
+    # return tasks_response
+
+
+    tasks = db.session.scalars(query).all()
+    return [t.to_dict() for t in tasks], 200
+
+
+@tasks_bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = get_task_by_id(task_id)
+    return task.to_dict(), 200
+
+
+@tasks_bp.put("/<task_id>")
+def update_task(task_id):
+    task = get_task_by_id(task_id)
+    data = parse_json_object()
+    try:
+        task.title = data["title"]
+        task.description = data["description"]
+    except KeyError:
+        return abort(make_response({"details": "Invalid data"}, 400))
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
+
+
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = get_task_by_id(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return Response(status=204, mimetype="application/json")
